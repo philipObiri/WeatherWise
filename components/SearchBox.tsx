4e0e0b3ac5a6dec639e21f39d88de702d13cb12d
Philip Obiri <philipobiri3.1@gmail.com>
2023-09-30 03:07:38 +0000

    feat: Add and Style A SearchBar component

---
+++ components/SearchBox.tsx
@@ -0,0 +1,8 @@
+export default function SearchBox() {
+    return (
+    <>
+    <input type="text" placeholder="🔎 City name" 
+    className="bg-gray-200 p-2 rounded-lg w-64"/>
+    </>
+    )
+   }
